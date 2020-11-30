fd43be01aec71033bef16fd3fee6375636aa5076
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-11-30 11:19:25 +0530

    minor changes

--- src/utils/map/exportMapData.ts
+++ src/utils/map/exportMapData.ts
@@ -1,15 +1,15 @@
-export const exportMapData = (map: any, mapDataDispDivId: string, layersData: object) => {
-    let mapData = { lines: [], circles: [] }
-    const layerKeys = Object.keys(layersData)
-    for (const lKey in layerKeys) {
+export const exportMapData = (map: any, mapDataDispDivId: string, shapesData: object) => {
+    let mapData = { lines: [], circles: [], labels: [] }
+    const layerKeys = Object.keys(shapesData)
+    for (const lKey of layerKeys) {
         if (!map._layers.hasOwnProperty(lKey)) {
             // if key is not present in map, discard this key
             continue
         }
-        var layerDataObj = JSON.parse(JSON.stringify(layersData[lKey]))
+        var layerDataObj = JSON.parse(JSON.stringify(shapesData[lKey]))
         layerDataObj["id"] = lKey
         var layer = map._layers[lKey]
-        if (layersData[lKey].type == 'polyline') {
+        if (shapesData[lKey].type == 'polyline') {
             const latLngs = layer._latlngs.map(function (latLng) {
                 return [latLng.lat, latLng.lng]
             })
@@ -20,7 +20,7 @@ export const exportMapData = (map: any, mapDataDispDivId: string, layersData: ob
             layerDataObj["latlngs"] = latLngs
             mapData.lines.push(layerDataObj)
         }
-        if (layersData[lKey].type == 'circle') {
+        else if (shapesData[lKey].type == 'circle') {
             const latLng = layer._latlng
             const radius = layer._radius
             if (radius == 0) {
@@ -30,6 +30,16 @@ export const exportMapData = (map: any, mapDataDispDivId: string, layersData: ob
             layerDataObj["latlng"] = [latLng.lat, latLng.lng]
             layerDataObj["radius"] = radius
             mapData.circles.push(layerDataObj)
+        }
+        else if (shapesData[lKey].type == 'text') {
+            const latLng = layer._latlng
+            const name = layerDataObj.meta.name
+            if (name == null || name == "") {
+                //name not present, discard
+                continue
+            }
+            layerDataObj["latlng"] = [latLng.lat, latLng.lng]
+            mapData.labels.push(layerDataObj)
         }
     }
     document.getElementById(mapDataDispDivId).textContent = JSON.stringify(mapData, undefined, 2)
